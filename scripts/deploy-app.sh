2e5ae953f234b1deca387f7489ef38cfccb0ba84
John Wiseheart <johnwiseheart@gmail.com>
2018-03-25 13:55:31 -0700

    Add ghpages single page app scripts

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -5,6 +5,9 @@ GITHUB_URL="https://${GH_TOKEN}@github.com/${USER_NAME}/${REPO_NAME}"
 
 git push -d $GITHUB_URL gh-pages
 
+echo "teledoodles.jcaw.me" > app/dist/CNAME
+cp app/404.html app/dist/404.html
+
 git add app/dist -f
 git commit --allow-empty -m "Commiting built assets for gh-pages"
 git subtree push --prefix app/dist $GITHUB_URL gh-pages
